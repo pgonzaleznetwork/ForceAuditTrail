b3974226247a79bd00c2705a4d3672f47f9fa0e3
Pablo Gonzalez <pgonzaleznetwork@gmail.com>
2024-07-03 11:33:24 +0100

    Added ValidationRule tests

--- __tests__/endToEnd.test.ts
+++ __tests__/endToEnd.test.ts
@@ -1,11 +1,27 @@
 import { parseAuditTrailRecord } from "../src";
-import { newValidationRule } from "./mockData/ValidationRule";
+import { newValidation, changedValidationMessage, removedValidation } from "./mockData/ValidationRule";
 
-describe('SetupAuditTrail', () => {
+describe('ValidationRule Tests', () => {
+
     test('New validation rule action is parsed correctly', () => {
-        const result = parseAuditTrailRecord(newValidationRule);
+        const result = parseAuditTrailRecord(newValidation);
         expect(result.metadataType).toBe('ValidationRule');
         expect(result.operationType).toBe('CREATED');
         expect(result.fieldModified).toBe(null);
     });
+
+    test('Changed validation rule message action is parsed correctly', () => {
+        const result = parseAuditTrailRecord(changedValidationMessage);
+        expect(result.metadataType).toBe('ValidationRule');
+        expect(result.operationType).toBe('MODIFIED');
+        expect(result.fieldModified).toBe('errorMessage');
+    });
+
+    test('Removed validation rule action is parsed correctly', () => {
+        const result = parseAuditTrailRecord(removedValidation);
+        expect(result.metadataType).toBe('ValidationRule');
+        expect(result.operationType).toBe('DELETED');
+        expect(result.fieldModified).toBe(null);
+    });
+
 });
--- __tests__/mockData/ValidationRule.ts
+++ __tests__/mockData/ValidationRule.ts
@@ -1,4 +1,4 @@
-export const newValidationRule = {
+export const newValidation = {
     "attributes": {
         "type": "SetupAuditTrail",
         "url": "/services/data/v60.0/sobjects/SetupAuditTrail/0YmKY00000100dl0AA"
@@ -14,4 +14,40 @@ export const newValidationRule = {
     "CreatedDate": "2024-06-27T10:13:42.000+0000",
     "Action": "newValidation",
     "Display": "New Accounts validation rule \"will_never_fire\""
+};
+
+export const changedValidationMessage = {
+    "attributes": {
+        "type": "SetupAuditTrail",
+        "url": "/services/data/v60.0/sobjects/SetupAuditTrail/0YmKY00000100dl0AA"
+    },
+    "Id": "0YmKY00000100dl0AA",
+    "CreatedBy": {
+        "attributes": {
+            "type": "User",
+            "url": "/services/data/v60.0/sobjects/User/0053h000002JF4cAAG"
+        },
+        "Username": "pgonzaleznetwork@brave-raccoon-mm7crl.com"
+    },
+    "CreatedDate": "2024-06-27T10:13:42.000+0000",
+    "Action": "changedValidationMessage",
+    "Display": `Changed error message for Accounts validation "Tamir_validation" from "Tamir is not a good name! jk, it's a great name" to "Tamir is not a good name! jk, it's a great name :)"`
+};
+
+export const removedValidation = {
+    "attributes": {
+        "type": "SetupAuditTrail",
+        "url": "/services/data/v60.0/sobjects/SetupAuditTrail/0YmKY00000100dl0AA"
+    },
+    "Id": "0YmKY00000100dl0AA",
+    "CreatedBy": {
+        "attributes": {
+            "type": "User",
+            "url": "/services/data/v60.0/sobjects/User/0053h000002JF4cAAG"
+        },
+        "Username": "pgonzaleznetwork@brave-raccoon-mm7crl.com"
+    },
+    "CreatedDate": "2024-06-27T10:13:42.000+0000",
+    "Action": "removedValidation",
+    "Display": 'Removed Accounts validation "will_never_fire"'
 };
